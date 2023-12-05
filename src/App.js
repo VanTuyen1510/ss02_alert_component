9e85e3e53fc683b984bd1f47712addb4d775bd67
VanTuyen1510 <tisiris1997@gmail.com>
2023-12-05 17:01:18 +0700

    alert component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyFunction from './components/Alert';
+
+function App() {
+  return (
+    <>
+    <MyFunction text = "Cảnh báo !Tài nguyên bạn vừa truy cập không tồn tại"></MyFunction>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+
+
+let MyFunction  = (props) =>{
+    return (
+    <div className="alert alert-warning" role = "alert">
+        {props.text}
+    </div>
+    )
+}
+export default MyFunction;
